95572b7d867f86f7c6ecd2f441b54abaa75e55d8
Caryn McCarthy <caryn.mccarthy@flatironschool.com>
2019-10-22 15:19:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,15 +25,15 @@ def move(board, index, character = "X")
 end
 
 def turn(board)
-puts "Please enter 1-9:"
-input = gets.strip
-index = input_to_index(input)
-if valid_move?(board, index) 
-move(board, index, "X")
-display_board(board)
-else
-turn(board)
-end
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index) 
+    move(board, index, "X")
+    display_board(board)
+  else
+    turn(board)
+  end
 end
   
    
